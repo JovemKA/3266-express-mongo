e72ac076319a96c834ab546f82732dff8ad0d889
Andrey Kaiky Reis Ferreira <Andrey.kaiky@outlook.com>
2024-10-03 19:35:31 -0300

    Criando os arquivos Schema e model de livros e realizando a primeira conexão com a collection livros.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin123@cluster0.qy006.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
 
     return mongoose.connection;
 };
